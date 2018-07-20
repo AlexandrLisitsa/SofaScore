7a8f1c0fc9f0a98f557d16181e6c57c54c7c0ba2
WyPik <wypik28@gmail.com>
2018-07-21 00:36:54 +0300

    add HTML Parser release getAllLinks method

---
+++ src/com/ria/sofascore/parser/HtmlParser.java
@@ -0,0 +1,35 @@
+package com.ria.sofascore.parser;
+
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+
+public class HtmlParser {
+
+    public ArrayList<String> getAllLinks(String document){
+        var links = new ArrayList<String>();
+        var doc = Jsoup.parse(document);
+        Elements eventList = doc.getElementsByAttributeValue("class", "js-event-list-table-container event-list");
+        eventList.forEach(el->{
+            Elements tournaments = el.getElementsByAttributeValue("class", "js-event-list-tournament tournament");
+            tournaments.forEach(t->{
+                Elements tournamentCategory = t.getElementsByAttributeValue("class", "tournament__category");
+                tournamentCategory.forEach(tc->{
+                    if(tc.text().equals("ITF Мужчины")||
+                            tc.text().equals("ITF Женщины")||
+                            tc.text().equals("Челленджер")||
+                            tc.text().equals("WTA")||
+                            tc.text().equals("APT")){
+                        Elements aClass = t.getElementsByAttributeValueContaining("class", "cell cell--event-list  pointer list-event js-event js-link");
+                        String href = aClass.attr("href");
+                        links.add(href);
+                    }
+                });
+            });
+
+        });
+        return links;
+    }
+
+}
